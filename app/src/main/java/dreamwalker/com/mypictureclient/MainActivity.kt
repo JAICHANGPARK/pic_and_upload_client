f96224ccbd8612f024d00e33e8afeec9a66e29ee
Dreamwalker <aristojeff@gmail.com>
2019-04-13 09:57:18 +0900

    Add project

---
+++ app/src/main/java/dreamwalker/com/mypictureclient/MainActivity.kt
@@ -0,0 +1,12 @@
+package dreamwalker.com.mypictureclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
