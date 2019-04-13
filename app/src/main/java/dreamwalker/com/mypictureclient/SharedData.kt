be534d4b62d280979802884f13eb85b564c71986
Dreamwalker <aristojeff@gmail.com>
2019-04-13 13:29:09 +0900

    Update Code

--- app/src/main/java/dreamwalker/com/mypictureclient/SharedData.kt
+++ app/src/main/java/dreamwalker/com/mypictureclient/SharedData.kt
@@ -1,5 +1,23 @@
 package dreamwalker.com.mypictureclient
 
+import android.content.Intent
+
 class SharedData {
 
+    val allModeSenderIntent = Intent()
+    val allModeProgressServiceIntent = Intent()
+    var isConnected : Boolean = false
+    val selectedModeSenderIntent = Intent()
+    val selectedModeProgressServiceIntent = Intent()
+    var allModeFileCount : Int = 0
+    var allModeTotalFileCount : Int = 0
+
+    var selectedModeFileCount : Int = 0
+    var selectedModeTotalFileCount : Int = 0
+
+    companion object{
+        val instant = SharedData()
+    }
 }
+
+
