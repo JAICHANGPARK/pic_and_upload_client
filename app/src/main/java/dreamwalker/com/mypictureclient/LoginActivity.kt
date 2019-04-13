4bef1dddfd10059fac13841da20b55974ae99694
Dreamwalker <aristojeff@gmail.com>
2019-04-13 12:08:07 +0900

    Update

--- app/src/main/java/dreamwalker/com/mypictureclient/LoginActivity.kt
+++ app/src/main/java/dreamwalker/com/mypictureclient/LoginActivity.kt
@@ -9,80 +9,78 @@ import android.os.Handler
 import android.os.Message
 import android.os.Process
 import androidx.appcompat.app.AlertDialog
-import androidx.core.content.getSystemService
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var mWifiManager: WifiManager
-    private lateinit var mWifiTextHandler : WifiStateHandler
+    private var mWifiStateThread: WifiStateThread? = null
+    private var mWifiTextHandler: WifiStateHandler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         mWifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         mWifiManager.isWifiEnabled = true
+
         mWifiTextHandler = WifiStateHandler()
 
+        mWifiStateThread = WifiStateThread()
+        mWifiStateThread?.start()
 
+    }
 
+    internal inner class WifiStateHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            if (msg.what == 0) {
+                if (wifi_state_info.text.length == 0 || wifi_state_info.text.length >= 15) {
+                    wifi_state_info.text = "WiFi 연결 시도 중"
+                } else {
+                    wifi_state_info.append(".")
+                }
+            } else if (msg.what == 1) {
+                wifi_state_info.text = "WiFi 연결 완료"
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                wifi_state_info.text = "WiFi 연결 실패"
+                val digBuilder = AlertDialog.Builder(this@LoginActivity)
+                    .setTitle("Wifi Error")
+                    .setMessage("연결할 수 있는 WiFi가 없습니다.\n" + "애플리케이션을 종료합니다.")
+                    .setPositiveButton(
+                        "확인"
+                    ) { dialog, which -> Process.killProcess(Process.myPid()) }
+                    .setIcon(R.mipmap.ic_launcher)
+                val dig = digBuilder.create()
+                dig.show()
+            }
+        }
     }
 
     private inner class WifiStateThread : Thread() {
         override fun run() {
             val start = System.currentTimeMillis()
             try {
-                mWifiTextHandler.sendEmptyMessage(0)
-                Thread.sleep(1500)
+                mWifiTextHandler?.sendEmptyMessage(0)
+                sleep(1500)
                 while (true) {
                     if (mWifiManager.connectionInfo.rssi > -70) {
-                        mWifiTextHandler.sendEmptyMessage(1)
+                        mWifiTextHandler?.sendEmptyMessage(1)
                         break
+                    } else {
+                        mWifiTextHandler?.sendEmptyMessage(0)
                     }
-                    else {
-                        //연결 시도
-                        mWifiTextHandler.sendEmptyMessage(0)
-                    }
+
+                    //10초
                     if (System.currentTimeMillis() - start >= 10000) {
+                        mWifiTextHandler?.sendEmptyMessage(2)
                         break
-                        //연결 실패
-                        mWifiTextHandler.sendEmptyMessage(2)
                     }
-                    Thread.sleep(100)
+                    sleep(100)
                 }
             } catch (e: Exception) {
-                print(e)
-            }
-        }
-    }
-
-    private inner class WifiStateHandler : Handler() {
-        override fun handleMessage(msg: Message?) {
-            if (msg?.what == 0) {
-                if (wifi_state_info.text.isEmpty() || wifi_state_info.text.length >= 15) {
-                    wifi_state_info.text = "Wi-Fi 연결 시도 중"
-                } else {
-                    wifi_state_info.append(".")
-                }
-            } else if (msg?.what == 1) {
-                wifi_state_info.text = "Wi-Fi 연결 완료"
-                val intent = Intent(applicationContext, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                wifi_state_info.text ="와이파이 연결 실패"
-                val dialogBuilder  = AlertDialog.Builder(this@LoginActivity)
-                    .setTitle("Wi-Fi Error")
-                    .setMessage("연결할 수 있는 Wi-Fi가 없습니다.")
-                    .setPositiveButton(android.R.string.ok){
-                        dialog, which -> Process.killProcess(Process.myPid())
-                    }
-                    .setIcon(R.mipmap.ic_launcher)
-
-                val dig = dialogBuilder.create()
-                dig.show()
-
             }
         }
     }
